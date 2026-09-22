3fd6cc61e98b03f462b960db5e9c967d00f1373d
limsokeng24-gif <limsokeng24@gmail.com>
2026-09-22 17:40:15 +0700

    Revert "Create & delete image in database and minio"
    This reverts commit b57c50408be3677e1835a450cc0b18ebe12bcf09.

--- src/main/java/com/school_management/overseas_language_centre/feature/core/TestImageService.java
+++
@@ -1,7 +0,0 @@
-package com.school_management.overseas_language_centre.feature.core;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface TestImageService {
-    void testImage(MultipartFile file);
-}
--- src/main/java/com/school_management/overseas_language_centre/feature/core/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.school_management.overseas_language_centre.feature.core.user.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UserService {
-    String uploadProfileImage(Long userId, MultipartFile file);
-    void deleteProfileImage(Long userId);
-}
